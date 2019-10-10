c4e94d4d64cebdca8475b88dee50e4b646cdb930
jcgomez <gb.jc@outlook.com>
2019-10-10 15:54:45 -0600

    246 - Detalles - instructor

--- src/app/components/pelicula/pelicula.component.ts
+++ src/app/components/pelicula/pelicula.component.ts
@@ -10,11 +10,19 @@ import { PeliculasService } from 'src/app/services/peliculas.service';
 export class PeliculaComponent implements OnInit {
 
   pelicula: any;
+  regresarA: string = '';
+  busqueda: string = '';
 
   constructor(public ar: ActivatedRoute, public ps: PeliculasService) {
     this.ar.params.subscribe(p => {
+      this.regresarA = p['pag'];
+
+      if (p['busqueda']) {
+        this.busqueda = p['busqueda'];
+      }
+
       this.ps.getPelicula(p['id']).subscribe(response => {
-        this.pelicula = response;
+        this.pelicula = response;        
       });
     });
   }
